3f91c56bdbe1a9b6ed3f71a2c56c45bd09ab54ce
mmatus0 <gonzalo.matus2201@alumnos.ubiobio.cl>
2025-11-13 14:08:58 -0300

    Correcciones generales

--- src/main/java/com/example/muebles/Entity/DetalleCotizacion.java
+++ src/main/java/com/example/muebles/Entity/DetalleCotizacion.java
@@ -1,12 +1,12 @@
 package com.example.muebles.Entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-
 @Data
+@Builder
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,19 +23,19 @@ public class DetalleCotizacion {
     private Cotizacion cotizacion;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cotizacion_id", nullable = false)
+    @JoinColumn(name = "variante_id", nullable = false)
     private Variante variante;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cotizacion_id", nullable = false)
+    @JoinColumn(name = "mueble_id", nullable = false)
     private Mueble mueble;
 
     @Column(name = "cantidad")
     private Integer cantidad;
     
     @Column(name = "precio_unitario")
-    private BigDecimal precioUnitario;
+    private Double precioUnitario;
     
     @Column(name = "subtotal")
-    private BigDecimal subtotal;
+    private Double subtotal;
 }
