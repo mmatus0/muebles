302533de2ad5db34fdcfa2389b8ba2568a5fd689
mmatus0 <gonzalo.matus2201@alumnos.ubiobio.cl>
2025-11-13 01:17:31 -0300

    Creación inicial de packages

---
+++ src/main/java/com/example/muebles/MueblesApplication.java
@@ -0,0 +1,13 @@
+package com.example.muebles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MueblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MueblesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/muebles/MueblesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muebles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MueblesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
